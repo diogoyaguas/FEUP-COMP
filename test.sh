5ea2c1ce51f74778a839d87c3bca4e57950ddace
PedroLopes <lopes.pedroa@gmail.com>
2019-05-24 15:19:47 +0100

    arguments testsh

--- test.sh
+++ test.sh
@@ -4,8 +4,8 @@ rm -rf bin
 mkdir bin
 javac -d bin src/*/*.java
 cd bin
-java src.AST.Program ../test-simple.txt
+java src.AST.Program ../test_files/$1
 java -jar ../jasmin.jar test-file.j
-java $1
+java $2
 printf "\n *** Run finished! ***\n\n"
-javap -c $1
+javap -c $2
