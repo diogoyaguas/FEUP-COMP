332133c674b0ddaed274f8d8948bfceeae793fc0
PedroLopes <lopes.pedroa@gmail.com>
2019-06-07 11:35:55 +0100

    added usage to script

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ $# != 1 ] 
+	then 
+        echo "One and only one argument must be provided!"
+		echo "Usage: run.sh [file name]"
+		exit 1
+	fi 
+
 rm -rf bin
 mkdir bin
 javac -d bin src/*/*.java
